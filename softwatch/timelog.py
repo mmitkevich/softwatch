944e2c8119a7fdcdd30179c63d91f4d4195ffb4b
mmitkevich <mikhail.mitkevich@gmail.com>
2015-03-11 17:27:11 +0300

    seems worth rewriting from scratch

--- softwatch/timelog.py
+++ softwatch/timelog.py
@@ -3,10 +3,18 @@ import time
 import os
 from subprocess import Popen, PIPE
 import re
+import timenode
+
+
 
 class TimeLog:
 
 
+    def __init__(self, dir, logfilename=None):
+        self.dir = dir
+        self.logfilename = logfilename or time.strftime("%Y-%m-%d.log")
+        self.curfilename = "active"
+        self.load_tasks()
 
 #33    #Taken from: http://stackoverflow.com/questions/3983946/get-active-window-title-in-x
     def get_active_window(self):
@@ -35,11 +43,13 @@ class TimeLog:
 
         return command
 
-    def escape(self, str):
+    @staticmethod
+    def escape(str):
         #Surround the string in double quotes and escape any pre-existing double quotes
         return '"%s"' % str.replace("\"", "\\\"")
 
-    def unescape(self, str):
+    @staticmethod
+    def unescape(str):
         #Remove surrounding double quotes and replace escaped double quotes
         m = re.match("^\"(.*)\"$", a)
         if m != None:
@@ -47,41 +57,95 @@ class TimeLog:
 
         return str.replace("\"", "\\\"")
 
-    def get_current_time(self):
+    @staticmethod
+    def get_current_time():
         return "%d" % (time.time() * 1000)
 
-    @staticmethod
-    def log_window(logfile, currentcommand, currentwindow):
-        with open(logfile, "aF+") as f:
-            moreinfo = ""
-            if re.search("chrom",currentcommand):
-                (moreinfo,err) =  Popen(['chromix', 'url'], stdout=PIPE).communicate()
+    def logtime(self,command = None,window=None):
+        command = command or self.currentcommand
+        window = window or self.currentwindow
+        f = open(os.path.join(self.dir,self.logfilename), 'aF+')
 
-            logstring = "%s %s %s %s\n" % (self.get_current_time(), self.escape(currentcommand), self.escape(currentwindow), self.escape(moreinfo))
+        moreinfo = ""
+        if re.search("chrom",command):
+            (moreinfo,err) =  Popen(['chromix', 'url'], stdout=PIPE).communicate()
+
+        tsk = self.query.process([TimeLog.get_current_time(),command,window,moreinfo],int(time.time() * 1000))
+        self.logtask(tsk)
+        logstring = "%s %s %s %s\n" % (TimeLog.get_current_time(), TimeLog.escape(command), TimeLog.escape(window), TimeLog.escape(moreinfo))
+        if f:
             f.write(logstring)
             f.flush()
-            print logstring
+            f.close()
+        print logstring+"-->"+(tsk.tag if tsk else "n/a")
+
+    @staticmethod
+    def fmt_delta_time(time):
+        time=int(time/1000)
+        days, remainder = divmod(time,3600*24)
+        hours, remainder = divmod(remainder, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        #s1 = '     ' if days<1 else "%3dd "%days
+        s1=""
+        s1 = s1+'%02d:%02d:%02d' % (hours, minutes,seconds)
+        return s1
+
+    def load_tasks(self):
+        self.query = timenode.TimeQuery(False,False)
+        self.query.tasks = timenode.loadTasks(os.path.join(self.dir,"tasks.cat"))
+        self.query.tasks.children.add(timenode.TimeNode("*OTHER*",expr=["*OTHER*"]))
+        self.query.tasks.children.add(timenode.TimeNode("*AWAY*",expr=["*OTHER*"]))
+
+    def logtask(self,tsk):
+        fname=os.path.join(self.dir, self.curfilename)
+        if not os.path.exists(fname):
+            self.load_tasks()
+
+
+        self.query.sort_tasks()
+        s = self.fmt_delta_time(self.query.tasks.time)+"="
+        k=0
+        rating={}
+        for node in self.query.tasks.children:
+            ttime = node.tqueue.sum()
+            alltime = self.query.tasks.tqueue.period#self.query.tasks.time
+            pct = 0. if self.query.tasks.time==0. else 100. * ttime/alltime
+            rating[pct]="%2d%%%s|" % (pct,node.tag)
+
+        for k in sorted(rating,rating.get,rating.get,True):
+            if k>0: s=s+" "+rating[k]
+
+        if len(s)>0:
+            print "SNAPSHOT: "+s
+            f = open(fname,"wt")
+            f.write(s)
+            f.close();
 
     def monitor_active_window(self):
         max_idle_timeout = 3
-        cur_idle = False
+        self.cur_idle = False
         lastwindow = None
-
+        i = 0
         try:
             while True:
-                curpid, currentwindow = self.get_active_window()
-                if currentwindow != lastwindow:
-                    currentcommand = self.get_process_command_from_id(curpid)
-                    self.log_window(self.logfile, currentcommand, currentwindow)
-                    lastwindow = currentwindow
+                self.curpid, self.currentwindow = self.get_active_window()
+                if self.currentwindow != lastwindow:
+                    self.currentcommand = self.get_process_command_from_id(self.curpid)
+                    self.logtime()
+                    lastwindow = self.currentwindow
                 idle = actmon.get_idle_time() > max_idle_timeout*1000
-                if idle!=cur_idle:
-                    cur_idle=idle
+                if idle!=self.cur_idle:
+                    self.cur_idle=idle
                     if idle:
-                        self.log_window(f, "<idle>", "idle for %d"%max_idle_timeout )
+                        self.logtime("<idle>", "idle for %d"%max_idle_timeout )
                     else:
-                        self.log_window(f, currentcommand, currentwindow)
+                        self.logtime()
+                i=i+1
+                if i>10:
+                    self.logtask(None)
+                    i=0
                 time.sleep(1)
+
         except KeyboardInterrupt:
-            self.log_window(f, 'activewindow', 'ActiveWindow terminated')
+            TimeLog.logtime('#SIGTERM', 'TimeLog terminated')
             print ""
--- softwatch/timenode.py
+++ softwatch/timenode.py
@@ -5,6 +5,36 @@ import time
 import shlex
 from sortedcontainers import SortedSet
 import re
+import traceback
+import math
+from collections import deque
+
+class TimeQueue:
+    def __init__(self,period):
+        self.q = deque()
+        self.period=period
+
+    def put(self,time,value):
+        self.q.append(dict(time=time,value=value))
+        if len(self.q)>0 and time<self.q[0]['time']:
+            print 'negative q'
+        while time-self.q[0]['time']>self.period:
+            self.q.popleft()
+        #if len(self.q)>0:
+        #    print TimeNode.fmt_time(time), (time-self.q[0]['time'])/1000/60.
+
+    def sum(self):
+        return reduce(lambda s,e: s+e['value'],self.q,0)
+
+
+    def time(self):
+        return self.q[-1]['value']
+
+    def pstart(self):
+        return self.q[0]['time']
+
+    def start(self):
+        return self.q[-1]['time']
 
 class TimeQuery:
     def __init__(self, min_time=60*1000,max_time=100*3600*1000,limit=1000000,max_depth=1000,samples=False,tree=True):
@@ -20,21 +50,126 @@ class TimeQuery:
         self.other = TimeNode("*OTHER*")
         self.min_start = 0
         self.max_start = sys.maxint
+        self.cats = None
+        self.away = TimeNode("*AWAY*")
+        self.total = TimeNode('*ONLINE*')
+        self.iline = 0
+        self.ptime = 0
+        self.pitems = []
+        self.pidle = 0
+        self.relative = False
+
+
+    def nperiods(self):
+        if not self.relative:
+            return 1.
+        return (self.total.time+self.away.time)*5./7./1000./3600/24
+
+    def process_file(self, in_file):
+        self.iline = 0
+        file = open(in_file,'r')
+
+
+        for line in file.readlines():
+            try:
+                items = shlex.split(line)
+                time = int(items[0])
+                if time<self.min_start:
+                    continue
+                if time>=self.max_start:
+                    break
+                self.process(items,time, 1000000000)
+
+            except BaseException as e:
+                print("%s(%d): syntax error %s "%(in_file,self.iline,e))
+                traceback.print_exc(e,sys.stdout)
+#               raise Error(e)
+
+    @staticmethod
+    def sample(start,time,taglist):
+        if len(taglist)==0:
+            print "empty taglist in sample"
+        return TimeSample(start, time, taglist)
+
+    def process(self, items, time, keeptime = 0):
+        self.iline += 1
+        if self.ptime==0:
+            self.ptime=time
+            self.pitems=items
+            return None
+        else:
+            tidle=0
+            if items[2].startswith("idle"):
+                self.pidle = time
+            else:
+                if self.pidle!=0:
+                    #if time-self.pidle<5*60*1000:
+                    #    return None
+                    tidle=time-self.pidle
+                    self.pidle = 0
+            awords = re.compile('[ /:?&|=\\,@#\]\[\(\)]+').split((self.pitems[2]+" "+self.pitems[3]).lower())
+            words = filter(lambda w: re.compile('[a-zA-Z]').search(w),awords)
+            #if len(awords)!=len(words):
+            #    print "1"
+            dt = time-self.ptime
+            if dt<0:
+                #print 'negative staart'
+                return
+            away_timeout=15*60*1000 # 15min max work per single program. rest time is 'away'
+            mytask=None
+            if tidle>0:
+                if keeptime>0:
+                    self.away.put([],self.sample(self.ptime,tidle,words)) #,str(int(dt/away_timeout))+'x'
+            elif (dt>away_timeout):
+                smpl = self.sample(self.ptime,away_timeout,words)
+                if keeptime>0:
+                    self.total.put(words, smpl)
+                if keeptime>0:
+                    self.away.put([],self.sample(self.ptime+away_timeout,dt-away_timeout,words)) #,str(int(dt/away_timeout))+'x'
+                mytask = smpl.task = self.tasks.putExpr("*AWAY*", smpl)
+            else:
+                smpl = self.sample(self.ptime,dt,words)
+                mytask = smpl.task = self.tasks.putExpr(words, smpl)
+                if keeptime>0:
+                    if mytask:
+                        words = ["["+mytask.tag+"]"]+words
+                    self.total.put(words, smpl,10)
+            self.ptime = time
+            self.pitems = items
+            return mytask
+
+    def sort_tasks(self):
+        temp = [ x for x in self.tasks.children]
+        self.tasks.children.clear()
+        for t in temp:
+            self.tasks.children.add(t)
+
 
 class TimeSample:
     def __init__(self, start = time.time(), time = 0, tags = []):
         self.time = time
         self.start = start
         self.tags = tags
+        self.task = None
 
 class TimeNode:
-    def __init__(self, tag, parent = None, time = 0, count = 0):
+    def __init__(self, tag, parent = None, time = 0, count = 0, tqueue = None,expr=[]):
         self.tag = tag
         self.count = count
         self.time = time
+        #self.wcount = wcount
         self.children = SortedSet(key=lambda node:-node.time)
         self.samples = []
         self.parent = parent
+        self.expr = expr
+        self.tqueue = TimeQueue(60*60*1000)
+        if tqueue:
+            self.tqueue.q.extend(tqueue.q)
+
+
+    #def alpha(self, tn, tp):
+    #    return 1-math.exp(-(tn-tp)/self.eperiod)
+
 
     def path(self):
         pth = []
@@ -44,6 +179,13 @@ class TimeNode:
             pth.insert(0,n)
         pth.append(self)
         return pth
+
+    @staticmethod
+    def delta(tag, a, b):
+        node = TimeNode(tag, None, a.time-b.time, a.count-b.count)
+        #node.wtime=[]
+        #node.wcount=a.wcount-b.wcount
+        return node
 
     def strpath(self, notext=True):
         if notext:
@@ -67,14 +209,28 @@ class TimeNode:
     #def __cmp__(self, other):
     #    return -(self.time-other.time)
 
+    def update_wtime(self, smpl):
+        self.tqueue.put(smpl.start,smpl.time)
+
+
     def put(self, taglist, sample, maxlevel=-1):
         if sample.time<0:
             print "negative time!"
+
+        alpha = 0.001 #self.alpha(sample.start,self.laststart)
 
         if len(taglist)==0 or maxlevel==0:
             self.samples.append(sample)
             self.count += 1
             self.time += sample.time
+
+            #try:
+            #    self.wcount = alpha*1+(1-alpha)*self.wcount
+            #    self.wtime = alpha*sample.time+(1-alpha)*self.wtime#sample.time
+            #except BaseException as e:
+            #    pass
+            self.update_wtime(sample)
+                #self.wcount=self.count
             return [1, sample.time]
 
         best_child = None
@@ -84,7 +240,7 @@ class TimeNode:
                 break
 
         if not best_child:
-            best_child = TimeNode(taglist[0],self)
+            best_child = TimeNode(taglist[0],self,0,0)
         else:
             self.children.remove(best_child)
 
@@ -92,9 +248,39 @@ class TimeNode:
         self.children.add(best_child)
         self.count += dc
         self.time += dtime
+        #self.wcount = alpha*dc+(1-alpha)*self.wcount
+        #self.wtime = alpha*dtime+(1-alpha)*self.wtime
+        #print self.tag,"=",self.wtime
+        self.update_wtime(sample)
         return [dc, dtime]
 
-    def fmt_delta_time(self,time):
+    def match(self, taglist):
+        for alt in self.expr:
+            if all(token in taglist for token in alt):
+                return True
+        return False
+
+    def putExpr(self, taglist, sample, keepSamples=False):
+        if sample.time<0:
+            print "negative time!"
+        res = None
+        for child in self.children:
+            if child.match(taglist):
+                if keepSamples:
+                    child.samples.append(sample)
+                child.count += 1
+                child.time += sample.time
+                self.count += 1
+                self.time += sample.time
+                self.tqueue.put(sample.start,sample.time)
+                res = child
+                break
+        if not res and taglist!="*OTHER*":
+            res=self.putExpr("*OTHER*",sample,keepSamples)
+        return res
+
+    @staticmethod
+    def fmt_delta_time(time):
         time=int(time/1000)
         days, remainder = divmod(time,3600*24)
         hours, remainder = divmod(remainder, 3600)
@@ -103,7 +289,8 @@ class TimeNode:
         s1 = s1+'%02d:%02d:%02d' % (hours, minutes, seconds)
         return s1
 
-    def fmt_time(self, start):
+    @staticmethod
+    def fmt_time(start):
         #return time.ctime(int(start/1000))
         return time.strftime("%Y/%m/%d %H:%M:%S", time.localtime(int(start/1000)))
 
@@ -114,98 +301,124 @@ class TimeNode:
                 return True
         return False
 
-    def query(self, tagset, options, indent=0):
+    def query(self, tagset, options, indent=0, orig_parent=None):
         next = self
+
+        if not orig_parent:orig_parent=options.total
+
         tagset = set(tagset)
         printnode = options.printnode if options.printnode else self.printnode
 
         self.checkremove(next.tag, tagset)
 
-        pathstr = next.strpath(options.tree)
-
-        while len(next.children)>0 and (next.time-next.children[0].time)<=options.min_time:
+        pathstr = next.strpath(options.tree>0)
+#        print "QUERY:",options.min_time,next.time
+        while len(next.children)>0 and ((next.time-next.children[0].time)<=0):# or (100.*next.children[0].time/options.total.time)>=100.-options.min_percent):
+#              print "joining "+options.min_time
               options.other.time+=next.time-next.children[0].time
               options.other.count+=next.count-next.children[0].count
+              #options.total.time+=next.time-next.children[0].time
+              #options.total.count+=next.count-next.children[0].count
               next = next.children[0]
               pathstr = pathstr+'-'+next.tag
               self.checkremove(next.tag, tagset)
 
-        if len(tagset)==0 and options.tree:
-            printnode(next, pathstr,options)
+        skip = False
+        if len(tagset)==0 and options.tree>0:
+            if  (next.time>=options.min_time*options.nperiods()):
+                printnode(next, pathstr,options, orig_parent)
+            else:
+                #printnode(next, "SKIP:"+pathstr,options, orig_parent)
+                if orig_parent.tag=="*ONLINE*":
+                    options.other.time=options.other.time+next.time
+                    options.other.count=options.other.count+next.count
+                skip=True
 
-        limit = options.limit
-        for child in next.children:
-          if indent<options.max_depth and limit>0 and (child.time>options.min_time):
-            child.query(tagset, options, indent+1)
-            limit-=1
-          else:
-            options.other.time+=child.time
-            options.other.count+=child.count
+        if not skip:
+            limit = options.limit
+            p = next if options.tree>0 else options.total
+            explained = 0.
 
-        if len(tagset)==0  and not options.tree and limit==options.limit:
-           options.rating.children.add(TimeNode(pathstr, None, next.time,next.count))
+            for child in next.children:
+              #explained=explained + 100.*child.time/p.time
+              if (options.tree==0 or indent<options.tree):# and (100.*child.time/options.total.time>options.min_percent):
+                child.query(tagset, options, indent+1,next)
+                limit-=1
+
+            #if len(tagset)==0  and options.tree==0 and limit==options.limit:
+            if len(tagset)==0  and options.tree==0 and (next.time>options.min_time*option.nperiods())  and limit==options.limit:
+               options.rating.children.add(TimeNode(pathstr, None, next.time,next.count,next.tqueue))
 
         if indent==0:
+            explained = 0
             for n in options.rating.children:
+                explaineds=explained + 100.*n.time/options.total.time
+                if explained>100.-options.min_percent:
+                    break
                 printnode(n,n.tag,options)
-            printnode(options.other, "*OTHER*", options)
-            printnode(self,"*TOTAL*",options)
+            printnode(options.other, None, options)
+            if 0==options.tree:
+                printnode(options.total,None,options)
+            printnode(options.away, None, options)
 
-    def sample(self,start,time,taglist):
-        return TimeSample(start, time, taglist)
+            options.sort_tasks()
+            print();
+            printnode(options.tasks,None,options)
+            for n in options.tasks.children:
+                if n.time>0:
+                    printnode(n,n.tag,options)
 
-    def printsample(self,samp, text):
-        print("%s | %s | %s"%(self.fmt_delta_time(samp.time),next.fmt_time(samp.start), ' '.join(samp.tags)))
 
-    def printnode(self, node, text, options = None):
-        print('%s | %5d | %s' % (self.fmt_delta_time(node.time), node.count, text))
+    @staticmethod
+    def printsample(samp, text):
+        tsk=""
+        if samp.task:
+            tsk = "["+samp.task.tag+"] "
+        print("|%s|%s|%s%s"%(TimeNode.fmt_delta_time(samp.time), TimeNode.fmt_time(samp.start), tsk,' '.join(samp.tags)))
 
-        printsample = options.printsample if options.printsample else self.printsample
+
+    @staticmethod
+    def printnode(node, text = None, options = None, parent=None):
+        if not text:
+            text = node.tag
+        percent = 100.
+        if options and options.total.time>0: #and 0==options.tree
+            percent = 100. * node.time / (options.total.time)
+        #if options and options.tree>0:
+        #    p = parent or options.total
+            #p = node.parent if node.parent else options.total
+            #while p.parent and 100.*node.time/p.time>=99-options.min_percent:
+            #    p=p.parent
+        #    percent = 100. * node.time / (p.time)
+
+        nperiods = options.nperiods()
+        stime=node.tqueue.sum()
+        print('%s|%s|%5d|%4.1f%%|%s' % (TimeNode.fmt_delta_time(int(node.time/nperiods)), TimeNode.fmt_delta_time(stime), node.count, percent, text))
+
+        printsample = options.printsample if options.printsample else TimeNode.printsample
         if options.samples:
             for samp in node.samples:
                 printsample(samp, ' '.join(samp.tags))
 
-    def read(self, in_file, options):
-        iline = 0
+
+
+def loadTasks(in_file):
+    tasks = TimeNode("=TASKS=")
+    try:
         file = open(in_file,'r')
-        ptime = 0
-        pitems = []
-        pidle=0
+
         for line in file.readlines():
-           iline += 1
-           try:
-            items = shlex.split(line)
-            time = int(items[0])
-            if time<options.min_start:
+            if line[0]=='#':
                 continue
-            if time>=options.max_start:
-                break
-            if ptime==0:
-                ptime=time
-                pitems=items
-            else:
-                if pitems[2].startswith("idle"):
-                    pidle = time
-                else:
-                    if pidle!=0:
-                        if time-pidle<5*60*1000:
-                            continue
-                        pidle=0
-                awords = re.compile('[ /:?&|=\\,@#\]\[\(\)]+').split((pitems[2]+" "+pitems[3]).lower())
-                words = filter(lambda w: re.compile('[a-zA-Z]').search(w),awords)
-                #if len(awords)!=len(words):
-                #    print "1"
-                dt = time-ptime
-                away_timeout=15*60*1000 # 15min max work per single program. rest time is 'away'
-                if dt>away_timeout:
-                    self.put(words, self.sample(ptime,away_timeout,words))
-                    self.put(['*AWAY*'],self.sample(ptime+away_timeout,dt-away_timeout,words)) #,str(int(dt/away_timeout))+'x'
-                else:
-                    self.put(words, self.sample(ptime,dt,words),10)
-
-                ptime = time
-                pitems = items
-
-           except BaseException as e:
-               print("%s(%d): syntax error %s "%(in_file,iline,e))
-
+            kv = line.split('==')
+            if len(kv)<2:
+                print "invalid category:\n"+line
+                continue
+            tag = kv[0].strip();
+            tn = TimeNode(tag)
+            tn.expr = [[y.strip() for y in x.split(' ') if len(y.strip())>0] for x in kv[1].split('||')]
+            tasks.children.add(tn)
+        file.close()
+    except BaseException as e:
+        print("failed to load tasks: %s"%(e))
+    return tasks
